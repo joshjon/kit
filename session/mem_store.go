4a483247b0d2ca08fa834a5b66668f6060f64c3c
joshjon <joshjon.dev@gmail.com>
2026-01-09 17:03:44 +1100

    Add session mem store secret hex decode

--- session/mem_store.go
+++ session/mem_store.go
@@ -1,10 +1,17 @@
 package session
 
 import (
+	"encoding/hex"
+	"fmt"
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/memstore"
 )
 
-func NewMemStore(secret string) sessions.Store {
-	return memstore.NewStore([]byte(secret))
+func NewMemStore(secret string) (sessions.Store, error) {
+	b, err := hex.DecodeString(secret)
+	if err != nil {
+		return nil, fmt.Errorf("hex decode session secret: %w", err)
+	}
+	return memstore.NewStore(b), nil
 }
